620994a47de0d10fa60bc6fb7622c01c2006a428
Ratish <ratish.jayemb@gmail.com>
2018-09-28 13:16:31 +0530

    Comlpeted Module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
- 
+
 import 'rxjs/add/operator/map';
 
-import {Album} from './album';
+import { Album } from './album';
 
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
-import {Product} from './product';
+import { Product } from './product';
 
 
 @Injectable()
@@ -19,14 +19,14 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number):Observable<Album> {
-    return this._http.get(this._albumUrl).map((response)=>
-    <Album>response.json());
-    };
-  
-  getProducts():Observable<Product[]>{
-    return this._http.get(this._productsUrl).map(response=>
-    <Product[]>response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) =>
+      <Album>response.json());
+  };
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) =>
+      <Product[]>response.json());
   }
-  }
+}
 
